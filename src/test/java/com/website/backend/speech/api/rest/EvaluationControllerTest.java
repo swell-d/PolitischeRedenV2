712ef80b18e90e56781e3457f87e3c7a8ed7ada8
Dimitri Rudenko <3952800@gmail.com>
2020-06-23 22:21:44 +0200

    Added test for EvaluationController

---
+++ src/test/java/com/website/backend/speech/api/rest/EvaluationControllerTest.java
@@ -0,0 +1,48 @@
+package com.website.backend.speech.api.rest;
+
+import com.website.backend.mock.MockPoliticalSpeechesFactory;
+import com.website.backend.mock.MockSpeakersFactory;
+import com.website.backend.speaker.db.SpeakerRepository;
+import com.website.backend.speech.db.PoliticalSpeechRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.text.ParseException;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EvaluationControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @TestConfiguration
+    static class CatalogTestConfiguration {
+        @Bean
+        public SpeakerRepository speakerRepository() {
+            return new MockSpeakersFactory().getSpeakerRepository();
+        }
+
+        @Bean
+        public PoliticalSpeechRepository politicalSpeechRepository() throws ParseException {
+            return new MockPoliticalSpeechesFactory().getRepository();
+        }
+    }
+
+    @Test
+    public void getProductFromCatalogTest() throws Exception {
+        this.mockMvc.perform(get("/evaluation")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
