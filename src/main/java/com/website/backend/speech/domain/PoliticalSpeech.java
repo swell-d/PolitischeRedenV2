dd2131d7c4b9e549649ee4a2c91329d6e5946ce6
Dimitri Rudenko <3952800@gmail.com>
2020-06-23 13:34:02 +0200

    Added fields to PoliticalSpeech class

--- src/main/java/com/website/backend/speech/domain/PoliticalSpeech.java
+++ src/main/java/com/website/backend/speech/domain/PoliticalSpeech.java
@@ -1,4 +1,10 @@
 package com.website.backend.speech.domain;
 
+import java.util.Calendar;
+
 public class PoliticalSpeech {
+    public String speaker;
+    public String topic;
+    public Calendar date;
+    public int words;
 }
--- src/test/java/com/website/backend/speech/domain/PoliticalSpeechTest.java
+++ src/test/java/com/website/backend/speech/domain/PoliticalSpeechTest.java
@@ -1,10 +1,30 @@
 package com.website.backend.speech.domain;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.GregorianCalendar;
+
+import static org.junit.Assert.assertEquals;
+
 public class PoliticalSpeechTest {
+
+    private PoliticalSpeech politicalSpeech;
+
+    @Before
+    public void setUp() throws Exception {
+        politicalSpeech = new PoliticalSpeech();
+    }
+
     @Test
-    public void shouldCreateObject() {
-        PoliticalSpeech politicalSpeech = new PoliticalSpeech();
+    public void shouldSaveValues() {
+        politicalSpeech.speaker = "John Smith";
+        politicalSpeech.topic = "Test topic";
+        politicalSpeech.date = new GregorianCalendar(2020, 5, 23);
+        politicalSpeech.words = 123;
+        assertEquals("John Smith", politicalSpeech.speaker);
+        assertEquals("Test topic", politicalSpeech.topic);
+        assertEquals(new GregorianCalendar(2020, 5, 23), politicalSpeech.date);
+        assertEquals(123, politicalSpeech.words);
     }
 }
