f7ad060c861b1e31a037d180faf19aaa92d65e4e
Dimitri Rudenko <3952800@gmail.com>
2020-06-24 19:59:48 +0200

    Refactoring

--- src/main/java/com/website/backend/config/BackendConfig.java
+++ src/main/java/com/website/backend/config/BackendConfig.java
@@ -2,26 +2,16 @@ package com.website.backend.config;
 
 import com.website.backend.speech.actions.PoliticianStatistic;
 import com.website.backend.speech.api.rest.StatisticController;
-import com.website.backend.speech.db.PoliticalSpeechRepository;
 import com.website.backend.speech.db.memory.InMemoryPoliticalSpeeches;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.text.ParseException;
 
 @Configuration
 public class BackendConfig {
 
     @Bean
-    public PoliticalSpeechRepository politicalSpeechRepository() throws ParseException {
-        return new InMemoryPoliticalSpeeches();
-    }
-
-    @Bean
-    public PoliticianStatistic politicianStatistic(
-            PoliticalSpeechRepository politicalSpeechRepository
-    ) {
-        return new PoliticianStatistic(politicalSpeechRepository);
+    public PoliticianStatistic politicianStatistic() {
+        return new PoliticianStatistic(new InMemoryPoliticalSpeeches());
     }
 
     @Bean
