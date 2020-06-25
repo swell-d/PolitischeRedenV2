d58102d1d3160cbb51fc85e9d73e464bc145569c
Dimitri Rudenko <3952800@gmail.com>
2020-06-25 11:33:07 +0200

    New tests

--- src/test/java/com/website/backend/speech/actions/CollectStatisticTest.java
+++ src/test/java/com/website/backend/speech/actions/CollectStatisticTest.java
@@ -1,30 +1,50 @@
 package com.website.backend.speech.actions;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class CollectStatisticTest {
-//    private CollectStatistic collectStatistic;
-//
-//    @Before
-//    public void setUp() throws Exception {
-//        PoliticalSpeechRepository speechRepository = new MockPoliticalSpeechesFactory().getRepository();
-//        collectStatistic = new CollectStatistic(speechRepository);
-//    }
-//
-//    @Test
-//    public void shouldReturnPoliticianWithTheGreatestNumberOfSpeechesIn2013() {
-//        assertEquals("Alexander Abel", collectStatistic.findPoliticianMostSpeechesInYear(2012));
-//        assertNull(collectStatistic.findPoliticianMostSpeechesInYear(2013));
-//    }
-//
-//    @Test
-//    public void shouldReturnMostSecurityPolitician() {
-//        assertEquals("Alexander Abel", collectStatistic.findPoliticianWithMostTopics("Innere Sicherheit"));
-//        assertNull(collectStatistic.findPoliticianWithMostTopics("Test topic"));
-//    }
-//
-//    @Test
-//    public void shouldReturnLeastWordyPolitician() {
-//        assertEquals("Caesare Collins", collectStatistic.findLeastWordyPolitician());
-//        CollectStatistic emptyDB = new CollectStatistic(new InMemoryPoliticalSpeeches());
-//        assertNull(emptyDB.findLeastWordyPolitician());
-//    }
+
+    private CollectStatistic statistic;
+
+    @Before
+    public void setUp() throws Exception {
+        statistic = new CollectStatistic();
+        statistic.addToStatistic(
+                "Alexander Abel",
+                "Bildungspolitik",
+                DateConverter.convertStringToCalendarFormat("2012-10-30"),
+                5310
+        );
+        statistic.addToStatistic(
+                "Bernhard Belling",
+                "Kohlesubventionen",
+                DateConverter.convertStringToCalendarFormat("2012-11-05"),
+                1210
+        );
+        statistic.addToStatistic(
+                "Caesare Collins",
+                "Kohlesubventionen",
+                DateConverter.convertStringToCalendarFormat("2012-11-06"),
+                1119
+        );
+        statistic.addToStatistic(
+                "Alexander Abel",
+                "Innere Sicherheit",
+                DateConverter.convertStringToCalendarFormat("2012-12-11"),
+                911
+        );
+    }
+
+    @Test
+    public void shouldCalculateStatistic() {
+        StatisticResponse expected = new StatisticResponse(
+                null,
+                "Alexander Abel",
+                "Caesare Collins"
+        );
+        assertEquals(expected, statistic.calculateResult());
+    }
 }
--- src/test/java/com/website/backend/speech/actions/PoliticianStatisticTest.java
+++ src/test/java/com/website/backend/speech/actions/PoliticianStatisticTest.java
@@ -2,21 +2,36 @@ package com.website.backend.speech.actions;
 
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
 public class PoliticianStatisticTest {
 
-    private final PoliticianStatistic politicianStatistic = new PoliticianStatistic();
+    private final PoliticianStatistic politicianStatistic = new PoliticianStatistic() {
+        @Override
+        protected BufferedInputStream getStream(URL url) throws IOException {
+            return new BufferedInputStream(new FileInputStream(new File("csv.csv")));
+        }
+    };
 
     @Test
-    public void shouldReturnStatisticResponse() {
+    public void shouldReturnStatisticResponse() throws MalformedURLException {
         StatisticResponse expected = new StatisticResponse(
                 null,
                 "Alexander Abel",
                 "Caesare Collins"
         );
-        assertEquals(expected, politicianStatistic.getStatistic(new ArrayList<>()));
+        ArrayList<URL> fakeUrls = new ArrayList<>() {{
+            add(new URL("http://localhost"));
+        }};
+        assertEquals(expected, politicianStatistic.getStatistic(fakeUrls));
     }
+
 }
