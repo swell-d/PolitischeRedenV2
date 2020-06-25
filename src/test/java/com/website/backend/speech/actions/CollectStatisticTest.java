2b764d16ebafeba3a361218f2f7cb1524e8b7c80
Dimitri Rudenko <3952800@gmail.com>
2020-06-25 13:03:38 +0300

    Bug fix

--- src/test/java/com/website/backend/speech/actions/CollectStatisticTest.java
+++ src/test/java/com/website/backend/speech/actions/CollectStatisticTest.java
@@ -12,10 +12,10 @@ public class CollectStatisticTest {
     @Before
     public void setUp() {
         statistic = new CollectStatistic();
-        statistic.addToStatistic("Alexander Abel", "Bildungspolitik", "2012-10-30", 5310);
-        statistic.addToStatistic("Bernhard Belling", "Kohlesubventionen", "2012-11-05", 1210);
-        statistic.addToStatistic("Caesare Collins", "Kohlesubventionen", "2012-11-06", 1119);
-        statistic.addToStatistic("Alexander Abel", "Innere Sicherheit", "2012-12-11", 911);
+        statistic.addToStatistic("Alexander Abel", "Bildungspolitik", "2012-10-30", "5310");
+        statistic.addToStatistic("Bernhard Belling", "Kohlesubventionen", "2012-11-05", "1210");
+        statistic.addToStatistic("Caesare Collins", "Kohlesubventionen", "2012-11-06", "1119");
+        statistic.addToStatistic("Alexander Abel", "Innere Sicherheit", "2012-12-11", "911");
     }
 
     @Test
